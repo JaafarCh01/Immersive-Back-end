d432400e92d3893e83ffb707e81e393a52e19c2b
Cherkaoui Jaafar <jaafarbuis55@gmail.com>
2024-08-06 10:33:22 +0100

    Fixed jwt

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
 import express from "express";
 import UserModel from "./model.js";
 import passport from "passport";
+import jwt from "jsonwebtoken"; // Import jsonwebtoken library
 import "./passport.js";
 
 const authRouter = express.Router();
@@ -21,55 +22,49 @@ authRouter.post("/register", async (req, res, next) => {
   }
 });
 
-
 authRouter.post("/login", async (req, res, next) => {
-    try {
-      //check if user exists
-      const userExists = await UserModel.findOne({ email: req.body.email });
-      if (!userExists)
-        return res.status(400).json({ message: "user does not exist" });
-  
-      // check if password is correct
-      if (userExists.password !== req.body.password)
-        return res.status(400).json({ message: "incorrect password" });
-  
-      // generate access token
-      const accessToken = jwt
-        .sign(
-          {
-            id: userExists._id,
-          },
-          "secret",
-          { expiresIn: "1d" }
-        )
-  
-      return res
-        .status(200)
-        .json({ message: "user logged in", accessToken: accessToken });
-    } catch (error) {
-      console.log(error);
-      next(error);
-    }
-  });
+  try {
+    //check if user exists
+    const userExists = await UserModel.findOne({ email: req.body.email });
+    if (!userExists)
+      return res.status(400).json({ message: "user does not exist" });
 
+    // check if password is correct
+    if (userExists.password !== req.body.password)
+      return res.status(400).json({ message: "incorrect password" });
 
-  authRouter.get("/profile", async (req, res, next) => {
-    try {
-      // check if user exists
-      const userExists = await UserModel.findOne({ email: req.body.email });
-      if (!userExists)
-        return res.status(400).json({ message: "user does not exist" });
-  
-      return res
-        .status(200)
-        .json({ userId: userExists._id, email: userExists.email });
-    } catch (error) {
-      console.log(error);
-      next(error);
-    }
-  });
+    // generate access token
+    const accessToken = jwt.sign(
+      {
+        id: userExists._id,
+      },
+      "mySuperSecretKey12345", 
+      { expiresIn: "1d" }
+    );
 
+    return res
+      .status(200)
+      .json({ message: "user logged in", accessToken: accessToken });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
+authRouter.get("/profile", async (req, res, next) => {
+  try {
+    // check if user exists
+    const userExists = await UserModel.findOne({ email: req.body.email });
+    if (!userExists)
+      return res.status(400).json({ message: "user does not exist" });
 
+    return res
+      .status(200)
+      .json({ userId: userExists._id, email: userExists.email });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
-  export default authRouter;
+export default authRouter;
